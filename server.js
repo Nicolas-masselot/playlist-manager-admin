e6507a05b24102f07da01ac8ad92529e5bd0cc99
nicolas-masselot <nicolasmasselot0@gmail.com>
2022-01-13 12:42:48 +0100

    tentative de résolution de l'erreur not found

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/playlistmanageradmin'));
+app.use(express.static('./dist/adminPlaylistManager'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/playlistmanageradmin/'}),
+    res.sendFile('index.html', {root: 'dist/adminPlaylistManager/'}),
 );
 
 // Start the app by listening on the default Heroku port
